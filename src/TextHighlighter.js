60f84841ad4a667267d878f7017a0ee4f169c36a
mirz <mirz.hq@gmail.com>
2014-12-24 21:48:33 +0100

    touchscreen support

--- src/TextHighlighter.js
+++ src/TextHighlighter.js
@@ -396,10 +396,12 @@
 
     function bindEvents(el, scope) {
         el.addEventListener('mouseup', scope.highlightHandler.bind(scope));
+        el.addEventListener('touchend', scope.highlightHandler.bind(scope));
     }
 
     function unbindEvents(el, scope) {
         el.removeEventListener('mouseup', scope.highlightHandler.bind(scope));
+        el.removeEventListener('touchend', scope.highlightHandler.bind(scope));
     }
 
     /**
